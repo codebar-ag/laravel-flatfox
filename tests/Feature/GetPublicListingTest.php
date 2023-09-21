71980bdd31d946117003591a27c7a1340a146310
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-09-21 14:31:29 +0100

    WIP

--- tests/Feature/GetPublicListingTest.php
+++ tests/Feature/GetPublicListingTest.php
@@ -3,10 +3,22 @@
 namespace CodebarAg\FlatFox\Tests\Feature;
 
 use CodebarAg\Flatfox\Requests\GetPublicListing;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
 
 it('get public listing', function () {
+
+    $mockClient = new MockClient([
+        GetPublicListing::class => MockResponse::fixture('get-public-listing-request'),
+    ]);
+
     $request = new GetPublicListing(142, '&expand=documents&expand=images');
+    $request->withMockClient($mockClient);
+
     $response = $request->send();
+
+    $mockClient->assertSent(GetPublicListing::class);
+
     expect($response->status())->toBe(200)
         ->and($response->dto())->not()->toBeEmpty();
 })
